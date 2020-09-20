4775ce0252dc8bf9b725eae038e53ecabcbdbe7b
Mochamad Arifin <klampok.child@gmail.com>
2020-09-20 12:07:10 +0700

    tab-selector and display admin error

--- home/login.php
+++ home/login.php
@@ -8,12 +8,12 @@
     <div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
         <!-- Nav tabs -->
         <ul class="nav nav-tabs" role="tablist">
-            <li role="presentation" class="active">
+            <li role="presentation" class="tab-warga">
                 <a href="#login-warga" aria-controls="login-warga" role="tab" data-toggle="tab">
                     <i class="fa fa-user"></i> Login Warga
                 </a>
             </li>
-            <li role="presentation">
+            <li role="presentation" class="tab-perangkat">
                 <a href="#login-perangkat" aria-controls="login-perangkat" role="tab" data-toggle="tab">
                     <i class="fa fa-user-circle"></i> Login Perangkat
                 </a>
@@ -24,14 +24,28 @@
         <div class="panel panel-default no-top-border">
             <div class="panel-body">
                 <div class="tab-content">
-                    <div role="tabpanel" class="tab-pane fade in active" id="login-warga">
+                    <div role="tabpanel" class="tab-pane fade tab-warga" id="login-warga">
                         <?php include __DIR__ . '/login_mandiri.php' ?>
                     </div>
-                    <div role="tabpanel" class="tab-pane fade" id="login-perangkat">
+                    <div role="tabpanel" class="tab-pane fade tab-perangkat" id="login-perangkat">
                         <?php include __DIR__ . '/login_perangkat.php' ?>
                     </div>
+                    
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script>
+(function(){
+    if(location.hash.startsWith('#perangkat')){
+        $('.tab-perangkat').addClass('in active');
+    }else{
+        //class="active" in active
+        $('.tab-warga').addClass('in active');
+    }
+    if( location.hash.endsWith("error")){
+        $('.tab-content').append('<div class="alert alert-warning" role="alert">Login gagal, cek username password anda.</div>')
+    }
+})();
+</script>
