0b8a3b326460bc9dffa3f42a2170f8c2da1417ea
Mochamad Arifin <klampok.child@gmail.com>
2024-10-12 08:20:16 +0700

    update theme match with v24

--- home/login.php
+++ home/login.php
@@ -1,39 +1,27 @@
+<?php
+$TITLE = sprintf('%s %s %s',
+            $this->setting->website_title,
+            ucwords($this->setting->sebutan_desa),
+            @$desa['nama_desa']
+);
+?>
 <header class="header-title">
     <div>
-        <img src="<?= $folder_themes ?>/img/logo-small.png">
         <h1>
-            <?= $TITLE ?>
+            Akses Login
         </h1>
     </div>
 </header>
 <div class="row">
-    <div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-        <!-- Nav tabs -->
-        <ul class="nav nav-tabs" role="tablist">
-            <li role="presentation" class="tab-warga">
-                <a href="#login-warga" aria-controls="login-warga" role="tab" data-toggle="tab">
-                    <i class="fa fa-user"></i> Login Warga
-                </a>
-            </li>
-            <li role="presentation" class="tab-perangkat">
-                <a href="#login-perangkat" aria-controls="login-perangkat" role="tab" data-toggle="tab">
-                    <i class="fa fa-user-circle"></i> Login Perangkat
-                </a>
-            </li>
-        </ul>
-
-        <!-- Tab panes -->
-        <div class="panel panel-default no-top-border">
-            <div class="panel-body">
-                <div class="tab-content">
-                    <div role="tabpanel" class="tab-pane fade tab-warga" id="login-warga">
-                        <?php include __DIR__ . '/login_mandiri.php' ?>
-                    </div>
-                    <div role="tabpanel" class="tab-pane fade tab-perangkat" id="login-perangkat">
-                        <?php include __DIR__ . '/login_perangkat.php' ?>
-                    </div>
-
-                </div>
+    <div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2  d-flex justify-content-center">
+        <div class="panel panel-warning">
+            <div class="panel-heading">
+                <img src="<?= $folder_themes ?>/img/logo-small.png">
+                <h3 class="panel-title text-center" style="display: inline"><?= $TITLE ?></h3>
+            </div>
+            <div class="panel-body text-center">
+                <a href="/layanan-mandiri" class="btn btn-lg btn-info"><i class="fa fa-user"></i> Login Warga</a>
+                <a href="/siteman" class="btn btn-lg btn-success"><i class="fa fa-user-circle"></i> Login Perangkat</a>
             </div>
         </div>
     </div>
--- section/footer.php
+++ section/footer.php
@@ -1,9 +1,8 @@
 <footer class="text-center container">
     <small>
         &copy; 2006-<?= date("Y") ?>
-        a Fork from <a target="_blank" href="https://github.com/OpenSID/OpenSID"> OpenSID
+        <a target="_blank" href="https://github.com/OpenSID/OpenSID"> OpenSID
             <?= AmbilVersi() ?></a>
-        &mdash; Retouch by <a target="_blank" href="https://klampok.id"> Klampok Dev Team</a>
     </small>
 </footer>
 </body></html>
